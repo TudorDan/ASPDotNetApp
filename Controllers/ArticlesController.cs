39d7761baabe0f560573d4b62c2e9879ba09d2f8
TudorDan <tudor.ist@gmail.com>
2023-08-29 02:07:41 +0300

    Add controllers

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,110 @@
+﻿using ASPDotNetApp.Daos;
+using ASPDotNetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetApp.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly IArticleDao _iarticleDao;
+
+        public ArticlesController(IArticleDao articleDao)
+        {
+            _iarticleDao = articleDao;
+        }
+
+        // GET: ArticlesController
+        public async Task<IActionResult> Index()
+        {
+            var articlesList = await _iarticleDao.GetAll();
+            return View(articlesList);
+        }
+
+        // GET: ArticlesController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArticlesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                var isCreateSuccessful = await _iarticleDao.Add(article);
+                if (isCreateSuccessful)
+                {
+                    TempData["success"] = $"Article {article.Name} created successfully";
+                }
+                else
+                {
+                    TempData["error"] = $"Error creating Article {article.Name}!";
+                }
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: ArticlesController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var articleFromDb = await _iarticleDao.Get(id);
+            if (articleFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(articleFromDb);
+        }
+
+        // POST: ArticlesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                var isUpdateSuccessful = await _iarticleDao.Update(article);
+                if (isUpdateSuccessful)
+                {
+                    TempData["succes"] = $"Article {article.Name} updated successfully";
+                }
+                else
+                {
+                    TempData["error"] = $"Error updating Article {article.Name}!";
+                }
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: ArticlesController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var articleFromDb = await _iarticleDao.Get(id);
+            if (articleFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(articleFromDb);
+        }
+
+        // POST: ArticlesController/DeletePost/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var isDeleteSuccessful = await _iarticleDao.Delete(id);
+            if (isDeleteSuccessful)
+            {
+                TempData["success"] = $"Article deleted successfully";
+            }
+            else
+            {
+                TempData["error"] = $"Article deleted successfully!";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/PriceListController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetApp.Controllers
+{
+    public class PriceListController : Controller
+    {
+        // GET: PriceListController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PriceListController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PriceListController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PriceListController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PriceListController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PriceListController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PriceListController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PriceListController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
