de8f22181cd9635879ffc4f7df2967fb2e09f9b4
TudorDan <tudor.ist@gmail.com>
2023-08-29 02:07:10 +0300

    Add Data Access Interfaces

---
+++ Daos/IArticleDao.cs
@@ -0,0 +1,13 @@
+﻿using ASPDotNetApp.Models;
+
+namespace ASPDotNetApp.Daos
+{
+    public interface IArticleDao
+    {
+        Task<IEnumerable<Article>> GetAll();
+        Task<bool> Add(Article article);
+        Task<bool> Update(Article article);
+        Task<Article?> Get(int id);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ Daos/IPriceListDao.cs
@@ -0,0 +1,9 @@
+﻿using ASPDotNetApp.Models;
+
+namespace ASPDotNetApp.Daos
+{
+    public interface IPriceListDao
+    {
+        Task<IEnumerable<PriceList>> GetAll(int articleId);
+    }
+}
