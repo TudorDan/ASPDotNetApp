2c1e9814dbe34a9744e98c822ddb9a5747fe0c38
TudorDan <tudor.ist@gmail.com>
2023-08-29 02:07:27 +0300

    Add Data Access Objects

---
+++ Daos/Impl/ArticleDao.cs
@@ -0,0 +1,206 @@
+﻿using ASPDotNetApp.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ASPDotNetApp.Daos.Impl
+{
+    public class ArticleDao : IArticleDao
+    {
+        private readonly IConfiguration _configuration;
+
+        public ArticleDao(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> Add(Article article)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = @"INSERT INTO Articles 
+                                (Name, Code, VATPercentage, PurchasePrice, RetailPrice, Status)
+                                VALUES (@Name, @Code, @VATPercentage, @PurchasePrice, @RetailPrice, @Status)
+                                SELECT SCOPE_IDENTITY()";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        command.Parameters.AddWithValue("@Name", article.Name);
+                        command.Parameters.AddWithValue("@Code", article.Code);
+                        command.Parameters.AddWithValue("@VATPercentage", article.VATPercentage);
+                        command.Parameters.AddWithValue("@PurchasePrice", article.PurchasePrice ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@RetailPrice", article.RetailPrice ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@Status", article.Status);
+                        int rowsAffected = await command.ExecuteNonQueryAsync();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = "DELETE FROM Articles WHERE Id = @Id";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        var rowsAffected = await command.ExecuteNonQueryAsync();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<Article?> Get(int id)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = @"SELECT * FROM Articles WHERE Id = @id";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                return new Article
+                                {
+                                    Id = id,
+                                    Name = reader.GetString(1),
+                                    Code = reader.GetString(2),
+                                    VATPercentage = reader.GetDecimal(3),
+                                    PurchasePrice = reader.IsDBNull(4) ? null : reader.GetDecimal(4),
+                                    RetailPrice = reader.IsDBNull(5) ? null : reader.GetDecimal(5),
+                                    Status = reader.GetBoolean(6)
+                                };
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Article>> GetAll()
+        {
+            var articlesList = new List<Article>();
+            try
+            {
+                using (var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = "SELECT * FROM Articles";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (reader.Read())
+                            {
+                                var article = new Article()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    Code = reader.GetString(2),
+                                    VATPercentage = reader.GetDecimal(3),
+                                    PurchasePrice = reader.IsDBNull(4) ? null : reader.GetDecimal(4),
+                                    RetailPrice = reader.IsDBNull(5) ? null : reader.GetDecimal(5),
+                                    Status = reader.GetBoolean(6)
+                                };
+                                articlesList.Add(article);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+            return articlesList;
+        }
+
+        public async Task<bool> Update(Article article)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = @"UPDATE Articles
+                                SET Name = @Name, Code = @Code, VATPercentage = @VATPercentage, 
+                                PurchasePrice = @PurchasePrice, RetailPrice = @RetailPrice, Status = @Status
+                                WHERE Id = @Id";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        command.Parameters.AddWithValue("@Id", article.Id);
+                        command.Parameters.AddWithValue("@Name", article.Name);
+                        command.Parameters.AddWithValue("@Code", article.Code);
+                        command.Parameters.AddWithValue("@VATPercentage", article.VATPercentage);
+                        command.Parameters.AddWithValue("@PurchasePrice", article.PurchasePrice ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@RetailPrice", article.RetailPrice ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@Status", article.Status);
+                        int rowsAffected = await command.ExecuteNonQueryAsync();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ Daos/Impl/PriceListDao.cs
@@ -0,0 +1,60 @@
+﻿using ASPDotNetApp.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ASPDotNetApp.Daos.Impl
+{
+    public class PriceListDao : IPriceListDao
+    {
+        private readonly IConfiguration _configuration;
+
+        public PriceListDao(IConfiguration iconfiguration)
+        {
+            _configuration = iconfiguration;
+        }
+
+        public async Task<IEnumerable<PriceList>> GetAll(int articleId)
+        {
+            var priceLists = new List<PriceList>();
+            try
+            {
+                using (var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    await conn.OpenAsync();
+                    var cmdTxt = @"SELECT * FROM PriceLists WHERE ArticleId = @articleId";
+                    using (var command = new SqlCommand(cmdTxt, conn))
+                    {
+                        command.Parameters.AddWithValue("@articleId", articleId);
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync()) 
+                            {
+                                var priceList = new PriceList
+                                {
+                                    Id = reader.GetInt32(0),
+                                    ArticleId = reader.GetInt32(1),
+                                    PurchasePrice = reader.IsDBNull(2) ? null : reader.GetDecimal(2),
+                                    MarkupPercentage = reader.IsDBNull(3) ? null : reader.GetDecimal(3),
+                                    ValidStartDate = reader.GetDateTime(4),
+                                    ValidEndDate = reader.GetDateTime(5),
+                                    RetailPrice = reader.IsDBNull(6) ? null : reader.GetDecimal(6)
+                                };
+                                priceLists.Add(priceList);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                await Console.Out.WriteLineAsync($"SQL Exception: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Generic Exception: {ex.Message}");
+                throw;
+            }
+            return priceLists;
+        }
+    }
+}
