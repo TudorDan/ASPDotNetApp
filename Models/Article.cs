bc4ccaf1d0b4b692b1c768f17b83fd969ff2cd34
TudorDan <tudor.ist@gmail.com>
2023-08-29 02:04:08 +0300

    Add model classes

---
+++ Models/Article.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPDotNetApp.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        [DisplayName("VAT percentage")]
+        [Range(0, 999.99, ErrorMessage = "VAT percentage must be between 0 and 999.99 only!")]
+        public decimal VATPercentage { get; set; }
+        [DisplayName("Purchase price")]
+        [Range(0, 9999999999999999.99, ErrorMessage = "Purchase price must be between 0 and 9999999999999999.99 only!")]
+        public decimal? PurchasePrice { get; set; }
+        [DisplayName("Retail price")]
+        [Range(0, 9999999999999999.99, ErrorMessage = "VAT percentage must be between 0 and 9999999999999999.99 only!")]
+        public decimal? RetailPrice { get; set; }
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ Models/PriceList.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPDotNetApp.Models
+{
+    public class PriceList
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        [DisplayName("Purchase price")]
+        public decimal? PurchasePrice { get; set; }
+        [DisplayName("Markup price")]
+        public decimal? MarkupPercentage { get; set; }
+        [Required]
+        [DisplayName("Valid start")]
+        public DateTime ValidStartDate { get; set; }
+        [Required]
+        [DisplayName("Valid end")]
+        public DateTime ValidEndDate { get; set; }
+        [DisplayName("Retail price")]
+        public decimal? RetailPrice { get; set; }
+    }
+}
